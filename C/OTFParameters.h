e650d42c60f3ebeb3e9a35c4acdd67dc79b8a4ac
Peter Teuben <teuben@gmail.com>
2020-12-23 19:44:59 -0500

    consistent ordering of parameter usage in the 7 places where this happens

--- C/OTFParameters.h
+++ C/OTFParameters.h
@@ -5,24 +5,29 @@
 #define N_INPUT_FILES 2000
 typedef struct
 {
+  // also set via command line options
   char i_filename[N_INPUT_FILES][OTF_FILE_NAME_LENGTH];
   int nfiles;
   char o_filename[OTF_FILE_NAME_LENGTH];
-  float rms_cutoff;
-  float noise_sigma;
   float resolution_size;
   float cell_size;
+  float rms_cutoff;
+  float noise_sigma;
+  float x_extent,y_extent;
+  int otf_select;
+  float rmax;
   int nsamples;
+  float otf_jinc_a, otf_jinc_b, otf_jinc_c;
+
+  // fixed in code
   int n_cell;
   int n_subcell;
   float model_spectrum_hpw, model_source_amp, model_source_hpw, model_source_x, model_source_y;
   int model_nchan;
-  float otf_jinc_a, otf_jinc_b, otf_jinc_c;
-  int otf_select;
-  float rmax;
-  float x_extent,y_extent;
   float sample_step;
   float scan_step;
+  
+  // derived in code
   int nx_samples, ny_samples;
   int use_pixels[16];
 } OTFParameters;
