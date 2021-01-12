cd4e75dac449a002ea1ddf206dbbd6e090f0a751
Peter Teuben <teuben@gmail.com>
2021-01-12 16:43:45 -0500

    allow multiple rc files

--- bin/lmtrc.py
+++ bin/lmtrc.py
@@ -13,28 +13,64 @@ import glob
 
 
 if len(sys.argv) < 3:
-    print("usage: %s rcfile key=val [key=val...]" % sys.argv[0])
+    print("usage: %s rcfile [rcfile2...] key=val [key=val...]" % sys.argv[0])
+    print("Existing keyword can change their value in the rcfile(s)")
+    print("For a single rcfile, output is stdout")
+    print("For multiple rcfile's, rcfile is changed")
     sys.exit(0)
 
 
-rcfile = sys.argv[1]
+nrc = 0    
+for av in sys.argv[1:]:
+    if os.path.exists(av):
+        nrc = nrc + 1
+    else:
+        break
+
+# print("Found %d RC files" % nrc)
+
+rcfiles = sys.argv[1:nrc+1]
+
+
 keyval = []
-for argv in sys.argv[2:]:
+for argv in sys.argv[nrc+1:]:
     kv = argv.split('=')
     if len(kv) == 2:
         keyval.append([kv[0],kv[1]])
 
 
-lines = open(rcfile).readlines()
-for line in lines:
-    line1 = line.strip()
-    n=0
-    for kv in keyval:
-        if line1.find("%s=" % kv[0])==0:
-            n = n + 1
-            print("%s=%s" % (kv[0],kv[1]))
-    if n==0:
-        print(line1)
+# print(keyval)
+
+if len(keyval)==0:
+    print("No keyword changes requested for the following rc files:")
+    print(rcfiles)
+    sys.exit(0)
+
+
+for rcfile in rcfiles:
+    lines = open(rcfile).readlines()
+    m=0
+    for i in range(len(lines)):
+        line1 = lines[i].strip()
+        n=0
+        for kv in keyval:
+            if line1.find("%s=" % kv[0])==0:
+                n = n + 1
+                m = m + 1
+                line2 = "%s=%s" % (kv[0],kv[1])
+                if nrc==1:
+                    print(line2)
+                else:
+                    lines[i] = line2
+        if n==0 and nrc==1:
+            print(line1)
+    if nrc > 1 and m>0:
+        print("Changing %s with %d change(s)" % (rcfile,m))
+        fp = open(rcfile,"w")
+        for line in lines:
+            fp.write("%s\n" % line.strip())
+        fp.close()
+        
         
         
 
