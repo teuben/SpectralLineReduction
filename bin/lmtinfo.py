0237c952a3b3e15a3bfaee59a82a0771e151a357
Peter Teuben <teuben@gmail.com>
2021-02-18 23:28:32 -0500

    add rsr, but need to deal with failures

--- bin/lmtinfo.py
+++ bin/lmtinfo.py
@@ -42,7 +42,11 @@ import netCDF4
 
 from docopt import docopt
 
-def summary(ifproc, rc=False):
+#  ifproc/ifproc_2018-06-29_078085_00_0001.nc
+#  spectrometer/roach0/roach0_78085_0_1_CHI-Cyg_2018-06-29_041713.nc
+#  RedshiftChassis0/RedshiftChassis0_2015-01-22_033551_00_0001.nc
+
+def slr_summary(ifproc, rc=False):
     """   summary a procnum in a single line
     """
     #   e.g.  M51_data/ifproc/ifproc_2020-02-20_091111_00_0001.nc
@@ -55,11 +59,13 @@ def summary(ifproc, rc=False):
     skyfreq  = nc.variables['Header.Sequoia.SkyFreq'][0]
     restfreq = nc.variables['Header.Sequoia.LineFreq'][0]
     bbtime = nc.variables['Data.IfProc.BasebandTime']
+    # Header.Dcs.ObsNum 
     obspgm = b''.join(nc.variables['Header.Dcs.ObsPgm'][:]).decode().strip()
     # the following Map only if obspgm=='Map'
-    xlen = nc.variables['Header.Map.XLength'][0] * 206264.806
-    ylen = nc.variables['Header.Map.YLength'][0] * 206264.806
-    hpbw = nc.variables['Header.Map.HPBW'][0]
+    if obspgm=='Map':
+        xlen = nc.variables['Header.Map.XLength'][0] * 206264.806
+        ylen = nc.variables['Header.Map.YLength'][0] * 206264.806
+        hpbw = nc.variables['Header.Map.HPBW'][0]
 
     date_obs = nc.variables['Data.TelescopeBackend.TelTime'][0].tolist()
     date_obs = datetime.datetime.fromtimestamp(date_obs).strftime('%Y-%m-%dT%H:%M:%S')
@@ -91,6 +97,27 @@ def summary(ifproc, rc=False):
         print("%s %s  %-5s %-20s %g %g %g" % (date_obs, fn[2], obspgm, src, restfreq, vlsr, dt))
 
 
+def rsr_summary(rsr_file, rc=False):
+    # RedshiftChassis2/RedshiftChassis2_2015-01-22_033551_00_0001.nc
+    nc = netCDF4.Dataset(rsr_file)
+
+    # Header.Source.SourceName
+    src = b''.join(nc.variables['Header.Source.SourceName'][:]).decode().strip()
+    
+    # Header.Dcs.ObsNum = 33551 ;
+    obsnum = nc.variables['Header.Dcs.ObsNum'][0] 
+    
+    # Header.Radiometer.UpdateDate = "21/01/2015 23:12:07
+    date_obs = b''.join(nc.variables['Header.Radiometer.UpdateDate'][:]).decode().strip()
+    
+    # Header.Weather.UpdateDate = "22/01/15 0:39:48
+    
+    nc.close()
+
+    # no rc mode, only one line summary
+    print("%s  %d  RSR   %s" %   (date_obs, obsnum, src))
+
+
 #  although we grab the command line arguments here, they are actually not
 #  used in the way most scripts use them. Below there is a more hardcoded
 #  parsing of arguments based on how many there are, which are files, and
@@ -109,10 +136,26 @@ if len(sys.argv) == 2:
 
     if os.path.isdir(ifproc):
         path = ifproc
-        fn = glob.glob('%s/ifproc/ifproc*.nc' % path)
+
+        chassis = -1
+        if chassis < 0:
+            globs = '%s/RedshiftChassis?/RedshiftChassis?_*.nc'  % (path)
+        else:
+            globs = '%s/RedshiftChassis%d/RedshiftChassis%d_*.nc'  % (path,chassis,chassis)            
+        fn = glob.glob(globs)
+        for f in fn:
+            # print('RSR',f)
+            try:
+                rsr_summary(f)
+            except:
+                print("Failing on ",f)
+            
+
+        globs = '%s/ifproc/ifproc*.nc' % path
+        fn = glob.glob(globs)
         for f in fn:
             try:
-                summary(f)
+                slr_summary(f)
             except:
                 try:
                     yyyymmdd = f.split('/')[-1].split('_')[1]
@@ -120,25 +163,32 @@ if len(sys.argv) == 2:
                     print("%s          %s   failed on %s" % (yyyymmdd,obsnum,f))
                 except:
                     print("1900-00-00       failed on %s" % f)
-                
+
         sys.exit(0)
     elif os.path.exists(ifproc):
         try:
-            summary(ifproc,rc=True)
+            slr_summary(ifproc,rc=True)
         except:
             print("%s: failed" % ifproc)
                
 elif len(sys.argv) == 3:
-                                                     # mode 2: path and obsnum
+                                                     # mode 2: path and obsnum : differentiate between SLR and RSR
     path = sys.argv[1]
     obsnum = sys.argv[2]
-    fn = glob.glob('%s/ifproc/ifproc*%s*.nc' % (path,obsnum))
+    globs = '%s/ifproc/ifproc*%s*.nc' % (path,obsnum)
+    fn = glob.glob(globs)
     if len(fn) > 0:
         ifproc = fn[0]
-        summary(ifproc,True)
+        slr_summary(ifproc,True)
     else:
-        print("Warning - no ifproc file found")
-        sys.exit(0)        
+        globs = '%s/RedshiftChassis?/RedshiftChassis?_*%s*.nc'  % (path,obsnum)
+        print("Trying RSR %s" % globs)
+        fn = glob.glob(globs)
+        if len(fn) > 0:
+            for f in fn:
+                print(f)
+        else:
+            print("Warning - no RSR files found")
 else:
                                                      # no other modes
     print("Usage : %s [path] obsnum" % sys.argv[0])
